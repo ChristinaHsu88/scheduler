76b1c5cf395e3da50dd608eda08d990ba22a630e
Christina Hsu <christina.hsu.ca@gmail.com>
2019-10-08 19:59:15 -0700

    modify the spot text rendering outcome

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./DayListItem.scss";
+import className from 'classnames';
 
 export default function DayListItem(props) {
+  let dayClass = className({'day-list__item--selected': props.name, 'day-list__item--full': props.spots === 0});
   return (
-    <li onclick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots > 1 ? props.spots + ' spots remaining' : props.spots === 0 ? 'no spots remaining' : props.spots + ' spot remaining'}</h3>
     </li>
   );
 }
