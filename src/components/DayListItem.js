9a53e3477f5b97be7c4dc9a0687242f6ea465c0a
Christina Hsu <christina.hsu.ca@gmail.com>
2019-10-10 13:36:23 -0700

    update the style to match the requirement

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./DayListItem.scss";
+// need to import this doc so we can access the stylesheet element
 import classNames from 'classnames';
 
 export default function DayListItem(props) {
-  const dayClass = classNames({'day-list__item--selected': props.name, 'day-list__item--full': props.spots === 0});
+  const dayClass = classNames({
+    'day-list__item--selected': props.spots > 0,
+    'day-list__item--full': props.spots === 0
+  });
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{props.spots > 1 ? props.spots + ' spots remaining' : props.spots === 0 ? 'no spots remaining' : props.spots + ' spot remaining'}</h3>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots > 1 ? props.spots + ' spots remaining' : props.spots === 0 ? 'no spots remaining' : props.spots + ' spot remaining'}</h3>
     </li>
   );
 }
