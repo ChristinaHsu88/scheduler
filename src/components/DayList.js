351ca0bed8edb12313174337b85428e4693a5868
Christina Hsu <christina.hsu.ca@gmail.com>
2019-10-10 10:52:02 -0700

    pass the setDay into DayList so now select day will render when being clicked

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,25 +3,6 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  // three props 1. days: Array, 2. day: String, 3. setDay: Function (accept the name of the day)
-  // responsible for rendering a list of DayListItem component
-  
-  // console.log(props); show entire props that got passed in
-  // let selectDay;
-  // props.days.map((day) => {
-    // if (day.name === props.day) {
-      // console.log(day.name)
-      // return (
-      //   <DayListItem 
-      //   key={day.id}
-      //   name={day.spots}
-      //   selected={day.name === props.day}
-      //   setDay={props.setDay}
-      // />
-      // )
-      // selectDay = day;
-    //}
-  //})
   return (
     <ul>
       {props.days.map((day) => {
@@ -30,6 +11,8 @@ export default function DayList(props) {
           key={day.id}
           name={day.name}
           spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
         />
         )
       })}
