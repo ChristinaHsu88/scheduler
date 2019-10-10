ed3ef74b28337aaee119de5186b850b34db2d70e
Christina Hsu <christina.hsu.ca@gmail.com>
2019-10-10 11:04:55 -0700

    add className method to make it work properly

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./DayListItem.scss";
-// import className from 'classnames'; for className method below
+import classNames from 'classnames';
 
 export default function DayListItem(props) {
-  // let dayClass = className({'day-list__item--selected': props.name, 'day-list__item--full': props.spots === 0}); need to fix this
+  const dayClass = classNames({'day-list__item--selected': props.name, 'day-list__item--full': props.spots === 0});
   return (
     <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots > 1 ? props.spots + ' spots remaining' : props.spots === 0 ? 'no spots remaining' : props.spots + ' spot remaining'}</h3>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{props.spots > 1 ? props.spots + ' spots remaining' : props.spots === 0 ? 'no spots remaining' : props.spots + ' spot remaining'}</h3>
     </li>
   );
 }
