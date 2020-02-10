02cc192c45b599721bac49839bb5527ed8b10373
Christina Hsu <christina.hsu.ca@gmail.com>
2020-02-10 12:25:54 -0800

    finish test button functions

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,24 @@
 import React from "react";
+import classNames from 'classnames';
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  // // let buttonClass = "button";
+
+  // if (props.confirm) {
+  //   buttonClass += " button--confirm"
+  // }
+
+  // if (props.danger) {
+  //   buttonClass += " button--danger"
+  // }
+  let buttonClass = classNames({
+    'button button--confirm': props.confirm,
+    'button button--danger': props.danger,
+    'button button': props
+  })
+  return <button className={buttonClass} 
+  disabled={props.disabled} 
+  onClick={props.onClick}>{props.children}</button>;
 }
